4caf8d68c814f005188a7dee9870bcb2f53e20d0
polisha <polinazuravlik2@gmail.com>
2026-04-06 22:00:25 +0300

    upd main

--- src/main.py
+++ src/main.py
@@ -3,21 +3,26 @@ from src.generator_source import GeneratorSource
 from src.task_source import TaskSource
 from src.logger import log
 
-sources = [
-    FileSource("data.json"),
-    GeneratorSource(3),
-    FileSource("bad.json"),
-    GeneratorSource(0),
-]
+def main():
 
-log.info("start")
-for src in sources:
-    print(f"\n{src.name}")
-    if not isinstance(src, TaskSource):
-        log.error(f"not a source: {type(src).__name__}")
-        print("not a source")
-        continue
-    tasks = list(src.get_tasks())
-    print(f"{len(tasks)} tasks")
-    log.info(f"ok: {src.name} - {len(tasks)} tasks")
-log.info("end")
+    sources = [
+        FileSource("data.json"),
+        GeneratorSource(3),
+        FileSource("bad.json"),
+        GeneratorSource(0),
+    ]
+
+    log.info("start")
+    for src in sources:
+        print(f"\n{src.name}")
+        if not isinstance(src, TaskSource):
+            log.error(f"not a source: {type(src).__name__}")
+            print("not a source")
+            continue
+        tasks = list(src.get_tasks())
+        print(f"{len(tasks)} tasks")
+        log.info(f"ok: {src.name} - {len(tasks)} tasks")
+    log.info("end")
+
+if __name__ == "__main__":
+    main()
