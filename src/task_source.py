31bad29b68a36503f43da477142989bb47787b5f
polisha <polinazuravlik2@gmail.com>
2026-05-28 17:43:18 +0300

    правки по ишью

---
+++ src/task_source.py
@@ -0,0 +1,8 @@
+from typing import Protocol, runtime_checkable, Iterable
+from src.task import Task
+
+@runtime_checkable
+class TaskSource(Protocol):
+    name: str
+    def get_tasks(self) -> Iterable[Task]: ...
+
